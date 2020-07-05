5b4a0b4d4b8a4738809da51b6faeb85dd5fc2474
i686leak <geraldoahnert@gmail.com>
2020-07-05 19:29:11 -0300

    Adicionando algumas partes do antigo Shell Script.

--- kml.rb
+++ kml.rb
@@ -2,8 +2,83 @@
 
 require 'colorize'
 require 'tty-prompt'
+require 'json'
 
-def install_tools
+def remove_repository
+
+	def user_accept_remove
+		system("clear && sed -i '/kali/d' /etc/apt/sources.list && sed -i '/Kali/d' /etc/apt/sources.list")
+		puts "Removed."
+		exit
+	end
+
+	remove_prompt = TTY::Prompt.new
+	remove_confirmation = remove_prompt.select('Do you want to remove Kali repository?'.magenta) do |menu|
+		menu.choice 'Yes'.green, 1
+		menu.choice 'No'.red, 2
+	end
+
+	remove_confirmation == 1 ? user_accept_remove : exit
+end
+
+def install_tools(type)
+
+	tools_all = File.read("tools/web.txt").split
+	tools_exploration = File.read("tools/exploration_tools.txt").split
+	tools_forensics = File.read("tools/forensics_tools.txt").split
+	tools_hardware = File.read("tools/hardware_hacking.txt").split
+	tools_infoget = File.read("tools/info_gethering.txt").split
+	tools_maintaining = File.read("tools/maintaining_acess.txt").split
+	tools_password = File.read("tools/password_atacks.txt").split
+	tools_reporting = File.read("tools/reporting_tools.txt").split
+	tools_reverse = File.read("tools/reverse_engineering.txt").split
+	tools_sniffing = File.read("tools/sniffing_and_spoofing.txt").split
+	tools_stress = File.read("tools/stress_testing.txt").split
+	tools_vul = File.read("tools/vul_analysis.txt").split
+	tools_web = File.read("tools/web.txt").split
+	tools_wireless = File.read("tools/wireless_atack.txt").split
+
+	def install_tools_command(type)
+		system("sudo apt-get install #{type.join}")
+	end
+
+	case type
+	when 1
+		install_tools_command(tools_all)
+	when 2
+		install_tools_command(tools_exploration)
+	when 3
+		install_tools_command(tools_forensics)
+	when 4
+		install_tools_command(tools_hardware)
+	when 5
+		install_tools_command(tools_infoget)
+	when 6
+		install_tools_command(tools_maintaining)
+	when 7
+		install_tools_command(tools_password)
+	when 8
+		install_tools_command(tools_reporting)
+	when 9
+		install_tools_command(tools_reverse)
+	when 10
+		install_tools_command(tools_sniffing)
+	when 11
+		install_tools_command(tools_stress)
+	when 12
+		install_tools_command(tools_vul)
+	when 13
+		install_tools_command(tools_web)
+	when 14
+		install_tools_command(tools_wireless)
+	else
+		system("clear")
+		puts "Invalid argument. Try again!"
+		exit		
+	end
+end
+
+def select_tools
 	system("clear")
 
 	tools_prompt = TTY::Prompt.new
@@ -23,6 +98,8 @@ def install_tools
 		menu.choice "Hardware Hacking".green, 13
 		menu.choice "Install All Tools".red, 14
 	end
+
+	install_tools(tools_category)
 end
 
 def install_repository(type_distro)
@@ -79,11 +156,11 @@ def options_menu(menu_choice)
 	when 1
 		detection_os()
 	when 2
-		# Install tools option.
+		install_tools()
 	when 3
-		# Remove repository.
+		remove_repository()
 	when 4
-		
+		exit
 	else
 		system("clear")
 		puts "Invalid argument. Try again!"
@@ -114,7 +191,9 @@ def inicial_menu
 						menu.choice 'Exit.'.cyan, 4
 	end
 	
-	return menu_choice
+	options_menu(menu_choice)
 end
 
-install_tools()
+def start_script
+	inicial_menu()
+end
