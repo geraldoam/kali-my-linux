c9f167031ce0eb2c503a75708944eadc52b5be0b
gerald0x01 <geraldoahnert@gmail.com>
2020-08-30 18:58:59 -0300

    Corrigindo erro no Arch-Based.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo "\e[36m
+printf "\e[36m
 
 
 
@@ -17,9 +17,12 @@ echo "\e[36m
               \e[0mgithub: gerald0x01
 "
 
-echo "\e[0mPlease, select your distribution to install script. Put the \e[1mnumber\e[0m.\n\n"
-echo "\e[36m[1]\e[0m Debian, Ubuntu, Linux Mint, Zorin OS."
-echo "\e[36m[2]\e[0m Arch Linux, Manjaro.\n"
+printf "\e[0mPlease, select your distribution to install script. Put the \e[1mnumber\e[0m."
+echo && echo
+printf "\e[36m[1]\e[0m Debian, Ubuntu, Linux Mint, Zorin OS."
+echo 
+printf "\e[36m[2]\e[0m Arch Linux, Manjaro."
+echo
 printf "\e[36m> \e[39m"
 read USER_DISTRIBUTION
 
@@ -29,9 +32,9 @@ if [ $USER_DISTRIBUTION = 1 ]; then
       sudo ln -sf /opt/kml/kml.rb /usr/bin/kml
       kml
 else
-      YAY_VERIFY=$(pacman -Qi yay)
+      YAY_EXIST='pacman -Qi yay'
       sudo pacman -S ruby
-      if [ $YAY_VERIFY -eq 0 ]; then
+      if [ -n "$YAY_EXIST" ]; then
             sudo pacman -S git
             git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
             sudo mv kml /opt/
