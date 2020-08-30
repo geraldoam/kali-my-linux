6598e0fe7208dbe409f1305b88cc94a9db69869a
gerald0x01 <geraldoahnert@gmail.com>
2020-08-30 19:30:48 -0300

    Corrigindo erro de instalação do yay.

--- install.sh
+++ install.sh
@@ -36,9 +36,10 @@ else
       sudo pacman -S ruby
       if [ -n "$YAY_EXIST" ]; then
             sudo pacman -S git
-            git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-            sudo mv kml /opt/
+            git clone https://aur.archlinux.org/yay.git && sudo chown $USER -R yay/ && cd yay && makepkg -si 
+            cd ..
+            sudo cp kml /opt/
             sudo ln -sf /opt/kml/kml.rb /usr/bin/kml
-            kml
+            sudo ruby /opt/kml/kml.rb
       fi
 fi
