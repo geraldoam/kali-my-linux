e5233913984883902a70c1ef73352a15c2eb05a9
gerald0x01 <geraldoahnert@gmail.com>
2020-09-01 15:04:41 -0300

    Adicionando sudo como flag.

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -17,10 +17,7 @@ echo "\e[36m
               \e[0mgithub: gerald0x01
 "
 sudo apt-get install ruby;
-# sudo cp -r kml /opt/;
-# sudo ln -sf /opt/kml/kml.rb /usr/bin/kml;
-# kml
 echo '#!/bin/bash' >> run.sh;
-echo 'ruby kml/kml.rb' >> run.sh;
+echo 'sudo ruby kml/kml.rb' >> run.sh;
 chmod +x run.sh;
 ./run.sh
