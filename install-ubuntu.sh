bee504dfefab3d25488e5a43c6a28184d8f00a5a
gerald0x01 <geraldoahnert@gmail.com>
2020-09-01 15:00:47 -0300

    Corrigindo erro de instalação do Ubuntu.

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -17,6 +17,10 @@ echo "\e[36m
               \e[0mgithub: gerald0x01
 "
 sudo apt-get install ruby;
-sudo cp -r kml /opt/;
-sudo ln -sf /opt/kml/kml.rb /usr/bin/kml;
-kml
+# sudo cp -r kml /opt/;
+# sudo ln -sf /opt/kml/kml.rb /usr/bin/kml;
+# kml
+echo '#!/bin/bash' >> run.sh;
+echo 'ruby kml/kml.rb' >> run.sh;
+chmod +x run.sh;
+./run.sh
